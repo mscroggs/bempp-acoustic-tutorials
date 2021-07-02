ed8649aed4b8c49deac3c8fd07f405308bd0e63b
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2021-07-02 11:52:20 +0100

    Update remove log messages script

--- .scripts/remove_log_messages.py
+++ .scripts/remove_log_messages.py
@@ -1,5 +1,5 @@
 import os
-import json
+import nbformat
 from sys import argv
 
 notebooks = []
@@ -8,13 +8,12 @@ for dir in ["../exercises", "../tutorials"]:
     for i in os.listdir(path):
         if i.endswith(".ipynb"):
             with open(os.path.join(path, i)) as f:
-                content = json.load(f)
+                content = nbformat.read(f, as_version=4)
 
             for n, c in enumerate(content["cells"]):
                 if c["cell_type"] == "code" and "outputs" in c:
                     content["cells"][n]["outputs"] = [
                         j for j in c["outputs"] if "name" not in j or j["name"] != "stderr"
                     ]
-
             with open(os.path.join(path, i), "w") as f:
-                json.dump(content, f)
+                nbformat.write(content, f)
